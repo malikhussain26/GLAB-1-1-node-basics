006bcf65088370a3cf9df5d2e8dff4b27147cb24
malikhussain26 <hussain.am2697@gmail.com>
2024-10-21 01:02:16 -0500

    final commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Part 4
 const http = require('http');
 const nodemon = require('nodemon');
 
@@ -15,3 +16,39 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostName, () => {
     console.log(`Server running at http://${hostName}:${port}/`);
 });
+
+// Part 5
+const url = require('url');
+
+const server2 = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+
+    if (pathname === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain'   
+ });
+        res.end('Hello, world!');
+    } else if (pathname === '/random') {
+        const randomNumber = Math.floor(Math.random() * 100) + 1;
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`A random number between 1 and 100: ${randomNumber}`);
+    } else if (pathname === '/greet') {
+        const name = parsedUrl.query.name;
+        if (name) {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(`Hello, ${name}!`);
+        } else {
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('Please provide a name.');
+        }
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not found');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server listening on port 3000');   
+
+});
+
